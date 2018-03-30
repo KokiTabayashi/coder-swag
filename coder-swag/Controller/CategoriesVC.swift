671b0169648c14fb59bd0e5d8373c1f6105a0817
Koki Tabayashi <koki@K-MacBook-Pro.local>
2018-03-29 23:06:44 -0700

    connecting data model with view

--- coder-swag/Controller/CategoriesVC.swift
+++ coder-swag/Controller/CategoriesVC.swift
@@ -10,8 +10,6 @@ import UIKit
 
 class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    
-
     @IBOutlet weak var categoryTable: UITableView!
     
     override func viewDidLoad() {
@@ -34,6 +32,21 @@ class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
         }
     }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {  // Not didDeselectRowAt
+        let category = DataService.instance.getCategories()[indexPath.row]
+        performSegue(withIdentifier: "ProductsVC", sender: category)
+    }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let productsVC = segue.destination as? ProductsVC {
+            let barBtn = UIBarButtonItem()
+            barBtn.title = ""
+            navigationItem.backBarButtonItem = barBtn
+            
+            assert(sender as? Category != nil)  // only for build time
+            productsVC.initProducts(category: sender as! Category)
+        }
+    }
+    
 }
 
