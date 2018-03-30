8149df2ad3b5c5eac7323a28204d93db0130a5a5
Koki Tabayashi <koki@K-MacBook-Pro.local>
2018-03-29 20:24:59 -0700

    collection view screen

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Koki Tabayashi on 2018/03/29.
+//  Copyright © 2018年 Koki Tabayashi. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
